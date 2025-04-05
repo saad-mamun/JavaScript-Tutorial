4d759975d3c2fad7af948d3c4e34c632af1d63e8
saad-mamun <mamunali88991@gmail.com>
2025-04-05 20:00:07 +0600

    seleting elements

---
+++ Dom/script.js
@@ -0,0 +1,13 @@
+const data = document.getElementById("getid")
+console.log(data)
+const demoClass = document.getElementsByClassName("getClass");
+console.log(demoClass)
+
+const tagCatch = document.getElementsByTagName("h3")
+console.log(tagCatch)
+
+const byName = document.getElementsByName("from")
+console.log(byName)
+
+const quary = document.querySelectorAll("*")
+console.log(quary)
