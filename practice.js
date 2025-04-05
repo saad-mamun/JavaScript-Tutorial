e456ff983a1e3f099c2e4b77f946def3ffe8cec1
saad-mamun <mamunali88991@gmail.com>
2025-04-05 15:34:30 +0600

    default paramiter

--- practice.js
+++ practice.js
@@ -1,21 +1,5 @@
-class car {
-    constructor(Brand){
-        this.Brand = Brand;
-    }
-    parent(){
-        return "My brand name is " + this.Brand
-    }
+// default paramiter
+function say(massage = "Hello Liton"){
+    console.log(massage)
 }
-
-class Model extends car{
-    constructor(Brand,Model){
-        super(Brand);
-        this.Model = Model;
-    }
-    show(){
-        return this.parent() + " and Model is " + this.Model
-    }
-}
-
-let Car = new Model("BMW",'x7');
-console.log(Car.show())
+say()
