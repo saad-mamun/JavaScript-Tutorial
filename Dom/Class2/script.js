1f1e52f45a7d5ce27a37704f1ad5c1b47738b9b7
saad-mamun <mamunali88991@gmail.com>
2025-04-07 22:41:57 +0600

    class-2nd

---
+++ Dom/Class2/script.js
@@ -0,0 +1,8 @@
+const note = document.querySelector(".note");
+console.log(note.parentNode)
+
+const content = document.getElementById("menu");
+console.log(content.firstElementChild)
+console.log(content.lastElementChild.previousElementSibling)
+// const child = content.firstChild.nodeName
+// console.log(child)
