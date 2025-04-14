626d956c60d6e3d53b01a5193202cfb4ec2279c4
saad-mamun <mamunali88991@gmail.com>
2025-04-14 21:57:04 +0600

    hashChange

--- Dom/Class-10/script.js
+++ Dom/Class-10/script.js
@@ -15,3 +15,32 @@ controller.addEventListener('click', function(e){
 
 });
 
+// focus
+const user = document.querySelector("#userid");
+user.addEventListener('focus', (e)=>{
+    e.target.style.backgroundColor='yellow'
+})
+user.addEventListener('blur',(e)=>{
+    e.target.backgroundColor=''
+})
+
+// hashchange
+
+const UpdateContent = () =>{
+    const content = document.querySelector("#content");
+    const hash = window.location.hash.substring(1);
+
+    switch(hash){
+        case "home":
+            content.innerHTML = "<h1>This is home page</h1>";
+            break;
+        case 'about':
+            content.innerHTML = '<h1>This is about page</h1>';
+            break;
+        case 'contact':
+            content.innerHTML = '<h1>This is contact page</h1>';
+            break;
+
+    }
+}
+window.addEventListener('hashchange',UpdateContent)
