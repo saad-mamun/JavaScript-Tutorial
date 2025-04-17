1c4c57ec2677cfbe82c931ef5b424c70612e8dbf
saad-mamun <mamunali88991@gmail.com>
2025-04-17 15:50:10 +0600

    CustomEvent

--- Dom/Class-11/script.js
+++ Dom/Class-11/script.js
@@ -24,3 +24,55 @@ myButton.addEventListener('click',()=>{
 })
 const event = new Event('click');
 myButton.dispatchEvent(event)
+
+
+
+//  JavaScript Custom Events
+
+document.addEventListener("Notify",()=>{  // 2nd para
+    alert("custom event fired")
+})
+
+
+const myBtn = document.querySelector("#myBtn"); //1st para
+
+myBtn.addEventListener('click',()=>{
+    let event = new CustomEvent("Notify");
+    document.dispatchEvent(event)
+})
+
+
+//CustomEvent Example
+
+ function highlight(elem) {
+            const bgColor = 'yellow';
+            elem.style.backgroundColor = bgColor;
+
+            // create the event
+            let event = new CustomEvent('highlight', {
+                detail: {
+                    backgroundColor: bgColor
+                }
+            });
+            // dispatch the event
+            elem.dispatchEvent(event);
+        }
+
+        // Select the div element
+        let div = document.querySelector('.note');
+
+        // Add border style
+        function addBorder(elem) {
+            elem.style.border = "solid 1px red";
+        }
+
+        // Listen to the highlight event
+        div.addEventListener('highlight', function (e) {
+            addBorder(this);
+
+            // examine the background
+            console.log(e.detail);
+        });
+
+        // highlight div element
+        highlight(div);
